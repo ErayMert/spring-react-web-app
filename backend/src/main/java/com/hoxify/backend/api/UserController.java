b4049ee4427dd799b75bb2d9cee9d468f77368ef
ErayMert <52412berkay>
2020-10-04 09:38:57 +0300

    kayıt verisi onayı için controller sınıfında error işlemleri yapıldı

--- backend/src/main/java/com/hoxify/backend/api/UserController.java
+++ backend/src/main/java/com/hoxify/backend/api/UserController.java
@@ -1,12 +1,17 @@
 package com.hoxify.backend.api;
 
 import com.hoxify.backend.entity.User;
+import com.hoxify.backend.error.ApiError;
 import com.hoxify.backend.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/1.0")
@@ -15,13 +20,21 @@ public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
-    UserService userService;
+    private UserService userService;
 
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user)
+    public ResponseEntity<?> createUser(@RequestBody User user)
     {
+        String username = user.getUsername();
 
-        log.info(user.toString());
+        if(username == null || username.isEmpty()){
+
+            ApiError apiError = new ApiError(400,"Validation Error", "/api/1.0/users");
+            Map<String,String> validationErros = new HashMap<>();
+            validationErros.put("username", "Username cannot be null");
+            apiError.setValidationErrors(validationErros);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+        }
 
         return ResponseEntity.ok(userService.save(user));
     }
