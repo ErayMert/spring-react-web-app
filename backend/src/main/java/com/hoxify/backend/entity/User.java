5e5ad33eb29e32de197e9fa16f0e89eecf6bfedd
ErayMert <52412berkay>
2020-10-11 15:58:24 +0300

    Validation annotation'ı ve username için bir annotation yazıldı

--- backend/src/main/java/com/hoxify/backend/entity/User.java
+++ backend/src/main/java/com/hoxify/backend/entity/User.java
@@ -1,10 +1,14 @@
 package com.hoxify.backend.entity;
 
+import com.hoxify.backend.annotation.UniqueUsername;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @ToString
 @Data
@@ -16,7 +20,18 @@ public class User {
     @Id
     @GeneratedValue(strategy =  GenerationType.AUTO)
     private long id;
+
+    @NotNull
+    @Size(min = 4, max = 100)
+    @UniqueUsername
     private String username;
+
+    @NotNull
+    @Size(min = 4, max = 100)
     private String displayName;
+
+    @NotNull
+    @Size(min = 8, max = 100)
+    @Pattern(regexp =  "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
     private String password;
 }
